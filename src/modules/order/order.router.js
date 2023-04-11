2367b2f7808438c91f0e65366d389287324ab1df
Mahmoud Elwan <mahmoudelwan460@gmail.com>
2023-04-11 16:22:10 +0200

    k

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -16,6 +16,6 @@ router.post('/',
 
 // ==========
 
-router.post('/webhook', express.raw({ type: 'application/json' }), webHook);
+router.post('/webhook', express.raw({ type: 'application/json' }), orderController.webHook);
 
 export default router
